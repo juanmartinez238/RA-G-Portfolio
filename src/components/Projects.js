2266a1c05aa59f3b31a68fd80fdb8f4024e78eb4
juanmartinez238 <juan.martinez238@yahoo.com>
2021-02-10 15:30:17 -0500

    completed landing page with animation and started projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {Tabs, Tab} from 'react-mdl';
 
-const Projects = () => {
-    return (
-        <div>
-            <h1>Projects</h1>
-        </div>
-    )
+class Projects extends Component{
+    constructor(props){
+        super(props);
+        this.state = {activeTab: 0};
+    }
+
+    toggleCategories(){
+        if(this.state.activeTab === 0){
+            return(
+                <div><h1>This is REact</h1></div>
+            )
+        }
+        // else if(this.state.activeTab === 1) {
+        //     return(
+        //         <div><h1>This is Other</h1></div>
+        //     )
+        // }
+    }
+
+    render(){
+        return(
+            <div className='category-tabs'>
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>
+                   <Tab>React</Tab> 
+                   {/* <Tab>Other</Tab> */}
+                </Tabs>
+                <section className='projects-grid'>
+                    {this.toggleCategories()}
+                </section>
+            </div>
+        )
+    }
 }
 
 export default Projects;
